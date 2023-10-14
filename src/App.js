bdc6ac03f8e702d437539bd940d959b0a568cc4e
code21112 <code21112@protonmail.com>
2023-10-14 18:36:04 +0200

    Added event handlers handleDecrement and handleIncrement

--- src/App.js
+++ src/App.js
@@ -5,13 +5,33 @@ class Counter extends React.Component {
     super(props);
 
     this.state = { counter: 0 };
+    this.handleIncrement = this.handleIncrement.bind(this);
+    this.handleDecrement = this.handleDecrement.bind(this);
   }
+
+  handleIncrement() {
+    // console.log("handleAdd triggered");
+    // console.log("this", this);
+    this.setState((curState) => {
+      return { counter: curState.counter + 1 };
+    });
+  }
+
+  handleDecrement() {
+    if (this.state.counter === 0) return;
+    this.setState((curState) => {
+      return {
+        counter: curState.counter - 1,
+      };
+    });
+  }
+
   render() {
     return (
       <div>
-        <button>+</button>
+        <button onClick={this.handleIncrement}>+</button>
         <span>{this.state.counter}</span>
-        <button>-</button>
+        <button onClick={this.handleDecrement}>-</button>
       </div>
     );
   }
